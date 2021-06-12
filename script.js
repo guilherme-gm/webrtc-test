3d8e3f37de2f234767371cf2ba977b7abcd592d2
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2021-06-12 01:25:26 -0300

    working

--- script.js
+++ script.js
@@ -1,74 +1,114 @@
-class Peer {
+class Host {
     constructor(name, server, type) {// host , guest
-        this.cfg = {'iceServers': [{"url": "stun:stun.xten.com"}]},
-        this.con = { 'optional': [{'DtlsSrtpKeyAgreement': true}] }
+        this.name = name;
+        this.server = server;
+    }
+
+    init = async () => {
+        this.conn = new RTCPeerConnection({ urls: "stun:stun.l.google.com:19302" });
+        this.conn.oniceconnectionstatechange = this.onIceConnectionStateChange;
+        this.conn.onicecandidate = this.onIceCandidate;
+        
+        this.dc = this.conn.createDataChannel('chat', { reliable: true });
+        this.dc.onopen = this.onChannelOpen;
+        this.dc.onmessage = this.onMessage;
+
+        // this offer is not the one used by the other client,
+        // we have to wait for the ICE Candidate
+        const offer = await this.conn.createOffer();
+        await this.conn.setLocalDescription(offer);
+    }
+
+    acceptConn = async (code) => {
+        const answer = new RTCSessionDescription(code);
+        await this.conn.setRemoteDescription(answer);
+        
+        console.log('acceptConn complete');
+    }
+/*
+    onMessage = (e) => {
+        console.log(`${this.name} onMessage`, e);
+    }
+*/
+    onIceCandidate = (e) => {
+        console.log('onicecandidate');
+        if (e.candidate) return;
+        document.getElementById("hostOffer").innerText = JSON.stringify(this.conn.localDescription);
+    }
+
+    onIceConnectionStateChange = (e) => {
+        console.log('on iec connection state change')
+        var state = this.conn.iceConnectionState;
+        console.log(state);
+        // $('#status').html(state);
+        if (state == "connected") {
+            console.log('good to send')
+        };
+    }
+
+    onChannelOpen = () => {
+        console.log('onChannelOpen');
+    }
+
+    onMessage = (e) => {
+        console.log(e);
+    }
+    
+    send = (msg) => {
+        this.dc.send(msg);
+    }
+
+}
+
+var sdpConstraints = { optional: [{RtpDataChannels: true}]  };
+
+class Guest {
+    constructor(name, server, type) {// host , guest
         this.name = name;
         this.server = server;
         this.type = type;
     }
 
-    init() {
-        this.conn = new RTCPeerConnection(this.cfg);
+    init = () => {
+        const conn = new RTCPeerConnection({ urls: "stun:stun.l.google.com:19302" })
+        conn.ondatachannel = (e) => { console.log('on data channel'); this.onDataChannel(e) };
+        conn.onicecandidate = this.onIceCandidate;
+        conn.oniceconnectionstatechange = (e) => { console.log('aaaa'); this.onIceConnectionStateChange(e) };
+        this.conn = conn;
         
-        this.sendChannel = this.conn.createDataChannel('sendDataChannel', { reliable: true });
-        this.sendChannel.onopen = this.onSendOpen;
-        this.sendChannel.onmessage = this.onMessage;
-        this.sendChannel.onclose = this.onSendclose;
-        console.log(`Created ${this.name} send channel`);
-
-        this.conn.onicecandidate = this.onicecandidate;
-        this.conn.ondatachannel = this.onDataChannel;
-        this.conn.onconnectionstatechange = ((ev) => console.log(ev))/
         console.log(`Created ${this.name} connection`);
-
     }
 
-    async createOffer() {
-        this.offer = await this.conn.createOffer();
-        await this.conn.setLocalDescription(this.offer);
-        console.log(`Offer for ${this.name}`, this.offer);
+    onIceCandidate = (e) => {
+        console.log('on ice candidate');
+        if (e.candidate) return;
+        document.getElementById("joinAnswer").innerText = JSON.stringify(this.conn.localDescription);
     }
 
-    async join(code) {
-        this.conn.setRemoteDescription(code);
-        this.answer = await this.conn.createAnswer();
-        await this.conn.setLocalDescription(this.answer);
-        console.log(' aq');
-        console.log(this.answer);
-        console.log(this.sendChannel);
+    onIceConnectionStateChange = (e) => {
+        console.log('onIceConnectionStateChange');
+        var state = this.conn.iceConnectionState
+        console.log(state);
+        if (state == "connected") console.log('good to msg');
     }
 
-    async acceptConn(code) {
-        await this.conn.setRemoteDescription(code);
-        
-        console.log(' aq');
-        console.log(this.offer);
-        console.log(this.sendChannel);
-    }
-
-    onMessage(e) {
-        console.log(`${this.name} onMessage`, e);
-    }
-
-    onIceCandidate(e) {
-        console.log(`${this.name} onIceCandidate`);
-
-    }
-
-    onSendOpen() {
-        console.log(`${this.name} onSendOpen`);
-    }
-
-    onSendclose() {
-        console.log(`${this.name} onSendclose`);
-    }
-
-    onDataChannel() {
+    onDataChannel = (e) => {
         console.log(`${this.name} onDatChanel`);
+        this.data = e.channel;
+        this.data.onopen    = () => console.log('connected');
+        this.data.onmessage = (e) => { if (e.data) console.log(e.data); }
     }
 
-    send(msg) {
-        this.sendChannel.send(msg);
+    join = async (code) => {
+        const offerDesc = new RTCSessionDescription(code);
+        this.conn.setRemoteDescription(offerDesc);
+        this.answer = await this.conn.createAnswer(sdpConstraints);
+        await this.conn.setLocalDescription(this.answer);
+        console.log("join complete");
+    }
+
+    send = (msg) => {
+        this.data.send(msg);
     }
 
 }
@@ -76,21 +116,15 @@ class Peer {
 let c;
 
 document.getElementById("host").onclick = async function() {
-    c = new Peer(document.getElementById("name").innerText, null);
-    c.init();
-    await c.createOffer();
-
-    document.getElementById("hostOffer").innerText = JSON.stringify(c.offer);
+    c = new Host(document.getElementById("name").innerText, null, 'host');
+    await c.init();
 }
 
 document.getElementById("join").onclick = async function() {
-    console.log(document.getElementById("joinCode"));
-    const answer = JSON.parse(document.getElementById("joinCode").value);
-    c = new Peer(document.getElementById("name").innerText, null);
-    c.init();
-    await c.join(answer);
-
-    document.getElementById("joinAnswer").innerText = JSON.stringify(c.answer);
+    const offerCode = JSON.parse(document.getElementById("joinCode").value);
+    c = new Guest(document.getElementById("name").innerText, null, 'guest');
+    await c.init();
+    await c.join(offerCode);
 }
 
 document.getElementById('accept').onclick = async function() {
